ab06fb114fc0feff0006959172103162716b9ece
Antony Sajan <antonysajant@gmail.com>
2025-03-12 22:09:41 +0530

    intialize hive

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,9 @@ import 'package:hive_flutter/hive_flutter.dart';
 
 void main() async{
 
+  WidgetsFlutterBinding.ensureInitialized();
   await Hive.initFlutter();
+  await Hive.openBox('subjectsBox');
   runApp(AttendanceManagerApp());
 }
 
